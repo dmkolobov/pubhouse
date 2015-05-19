fe564987ef2ebc86fddc1ea51ff189169a16d488
Dmitry Kolobov <dmkolobov@gmail.com>
2015-05-19 13:29:05 -0600

    Rewrote the core to be simpler.

--- src/pubhouse/core.clj
+++ src/pubhouse/core.clj
@@ -1,137 +1,84 @@
 (ns pubhouse.core
-  (:require [clojure.java.io :refer [as-file]]
-            [clojure.string :refer [join]]
-            [markdown.core :refer [md-to-html-string]]
-            [hiccup.core :as hiccup]
+  (:require [clojure.string :refer [join]]
             [me.raynes.fs :as fs]))
 
-(def ^:dynamic *source-file?* nil)
-(def ^:dynamic *render* nil)
-(def ^:dynamic *meta-sep*  "===")
-
 (defn relative-path
-  "Takes a path as the sole argument and returns path with any parts
-  matching the current *cwd* removed. Requires path to be a child of 
-  *cwd*."
+  "Returns the sub-string of path relative to the current-value of *cwd*.
+  Assumes that path is a equal to or a child of *cwd*"
   [path]
-  (->> (fs/split path)
-       (drop (count (fs/split fs/*cwd*)))
-       (clojure.string/join "/")))
+  (->> (fs/split path) (drop (count (fs/split fs/*cwd*))) (join "/")))
 
-(defn ensure-parent-dir
-  "Invoke the function f, ensuring that the parent directory of the file at path
-  exists. Presumabely, f has side-effects involving writing to the file at path."
-  [path f]
-  (let [parent (fs/parent path)]
-    (if (fs/exists? parent)
-      (f)
-      (do (fs/mkdirs parent) (f)))))
+(defn strip-extensions
+  "Strips all extensions from the path string."
+  [path]
+  (if-let [ext (fs/extension path)]
+    (recur (clojure.string/replace path ext ""))
+    path))
 
-(def content-file?
-  "Any files for which this predicate returns true will be compiled."
-  (complement fs/directory?))
-
-(defn get-file-info
-  [file]
-  {:path (relative-path (.getPath file))
-   :mod-time (fs/mod-time file)})
-
-(defn get-page-info
-  "Reads the lines preceding the *meta-sep* line as a persistent map."
-  [lines]
-  (->> lines (take-while #(not= % *meta-sep*)) (join "\n") (read-string)))
-
-(defn content-section
-  "Returns a lazy sequence of lines following the *meta-sep* line."
-  [lines]
-  (->> lines (drop-while #(not= % *meta-sep*)) (drop 1)))
-
-(defn strip-extension
-  [s]
-  (if-let [e (fs/extension s)] (clojure.string/replace s e "") s))
-
-(defn with-io
-  "Creates a reader and writer using the input and output files, and invokes the 
-  f with the arguments (reader writer). Ensures that the parent directory of the output
-  file exists."
-  [input output f]
-  (ensure-parent-dir output
-    (fn []
-      (with-open [reader (clojure.java.io/reader input)
-                  writer (clojure.java.io/writer output)]
-        (f reader writer)))))
-
-(defn input-file
-  "Give a path to the root of the site directory and the relative path
-  of a resource source file, return that resource source file."
-  [site-root rel-path]
-  (fs/with-cwd site-root
-    (fs/with-cwd "content"
-      (fs/file rel-path))))
-
-(defn output-file
-  "Given a path to the root of the build directory and the URL of
-  a resource, return thefile to be used for output of resource compilation."
-  [build-path url]
-  (fs/with-cwd build-path
-    (fs/file (str url ".html"))))
+(defn path->url
+  "Convert a path to a url, with paths relative to the current value of *cwd*"
+  [path]
+  (-> path (relative-path) (strip-extensions) (str ".html")))
 
 (defn canonical-url
+  "Given some url, ensures that it is an absolute url. If url is a path to
+  some index.html, return a url to the parent directory."
   [url]
-  (let [url (str "/" url)]
-    (clojure.string/replace url "index" "")))
+  (str "/" (-> url (clojure.string/replace "index.html" "") (strip-extensions))))
+
+(defn file-mapping
+  [root file]
+  (let [path (.getPath file)]
+    (fs/with-cwd root [path (path->url path)])))
+
+(defn site-mapping
+  [source-file? root-path]
+  (let [root (fs/file root-path)]
+    (->> (file-seq root)
+         (filter source-file?)
+         (map (partial file-mapping root)))))
+
+(defn read-info
+  "Reads the lines preceding the info block separator '===' in as a 
+  Clojure data structure."
+  [reader]
+  (let [not-sep #(not= % "===")]
+    (->> (line-seq reader) (take-while not-sep) (join "\n") (read-string))))
 
 (defn make-page
-  [file-info page-info]
-  (merge page-info
-         {:url (canonical-url
-                (strip-extension
-                 (:path file-info)))}))
+  [path url]
+  {:url (canonical-url url)
+   :info (with-open [reader (clojure.java.io/reader (fs/file path))]
+           (read-info reader))})
 
-(defn compile-file!
-  [{:keys [site-path build-path]} site [file-info page-info]]
-  (let [path (:path file-info)
-        input (input-file site-path path)
-        output (output-file build-path (strip-extension path))]
-    (with-io input output
-      (fn [reader writer]
-        (.write writer
-                (*render* file-info
-                          (assoc site
-                                 :current-page
-                                 (assoc (make-page file-info page-info)
-                                        :content-lines
-                                        (content-section (line-seq reader))))))))))
+(defn resource-key
+  [url]
+  (-> url (strip-extensions) (fs/split)))
 
-(defn build-site-map!
-  [options]
-  (fs/with-cwd (:site-path options)
-    (fs/with-cwd "content"
-      (doall
-       (for [file (->> (fs/file ".") (file-seq) (filter *source-file?*))]
-         (with-open [reader (clojure.java.io/reader file)]
-           [(get-file-info file) (get-page-info (line-seq reader))]))))))
-
-(defn content->site
-  [site-map]
-  (reduce (fn [site [file-info page-info]]
-            (assoc-in site
-                      (fs/split
-                       (strip-extension (:path file-info)))
-                      (make-page file-info page-info)))
+(defn mapping->site
+  [mapping]
+  (reduce (fn [site [path url]]
+            (assoc-in site (resource-key url) (make-page path url)))
           {}
-          site-map))
-  
-(defn compile-content!
-  [options site-map]
-  (let [site (content->site site-map)]
-    (doseq [content-map site-map]
-      (compile-file! options site content-map))))
+          mapping))
 
-(defn build-site
-  [options source-file? render]
-  (let [options (assoc options :root fs/*cwd*)]
-    (binding [*source-file?* source-file?
-              *render* render]
-      (compile-content! options (build-site-map! options)))))
+(defn html-writer
+  [build-root url]
+  (let [file (fs/with-cwd build-root (fs/file url))
+        parent (fs/parent file)]
+    (when (not (fs/exists? parent)) (fs/mkdirs parent))
+    (clojure.java.io/writer file)))
 
+(defn compile-site
+  [f {:keys [site-root build-root source-file?]}]
+  (let [site-map (site-mapping source-file? site-root)
+        site (mapping->site site-map)]
+    (doseq [[path url] site-map]
+      (with-open [reader (clojure.java.io/reader (fs/file path))
+                  writer (html-writer build-root url)]
+        (f (assoc site
+                  :current-page
+                  (get-in site (resource-key url)))
+           reader
+           writer)))))
+      
--- src/pubhouse/play.clj
+++ src/pubhouse/play.clj
@@ -2,7 +2,7 @@
   (:require [hiccup.core :as hiccup]
             [markdown.core :as md]
             [me.raynes.fs :as fs]
-            [pubhouse.core :as pb]))
+            [pubhouse.core :refer [compile-site]]))
 
 (defn nav-item
   [current-url url label]
@@ -17,10 +17,10 @@
    (->> (seq site)
         (map (fn [[name data]]
                (if-let [url (:url data)]
-                 [:li (nav-item current-url url (:title data))]
+                 [:li (nav-item current-url url (get-in data [:info :title]))]
                  (when-let [index (get data "index")]
                    [:li
-                    (nav-item current-url (:url index) (:title index))
+                    (nav-item current-url (:url index) (get-in index [:info :title]))
                     (top-nav current-url (dissoc data "index"))])))))])
 
 (defn play-template
@@ -28,28 +28,29 @@
   (hiccup/html
    [:html
     [:head
-     [:title (get-in site [:current-page :title])]
+     [:title (get-in site [:current-page :info :title])]
      [:link {:rel "stylesheet" :src "style.css"}]]
     [:body
      (top-nav (get-in site [:current-page :url]) (dissoc site :current-page))
-     [:h1 (get-in site [:current-page :title])]
+     [:h1 (get-in site [:current-page :info :title])]
      content
      [:div.development
       [:pre {:style "font-family: Courier;line-height: 1.5em;"}
        (with-out-str (clojure.pprint/pprint site))]]]]))
 
-(defn md-site->html
-  [site]
-  (md/md-to-html-string
-   (clojure.string/join "\n" (get-in site [:current-page :content-lines]))))
+(defn read-content
+  [reader]
+  (->> (line-seq reader)
+       (drop-while #(not= % "==="))
+       (drop 1)
+       (clojure.string/join "\n")
+       (md/md-to-html-string)))
 
 (defn play
   []
-  (pb/build-site
-   {:site-path "example"
-    :build-path "example-build"}
-   (fn [file]
-     (= ".md" (fs/extension file)))
-   (fn [file-info site]
-     (when (= ".md" (fs/extension (:path file-info)))
-       (play-template site (md-site->html site))))))
+  (compile-site (fn [site reader writer]
+                  (.write writer
+                          (play-template site (read-content reader))))
+                {:site-root "example/content"
+                 :build-root "example-build"
+                 :source-file? (fn [f] (= ".md" (fs/extension f)))}))
